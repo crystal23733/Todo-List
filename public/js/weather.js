cadcdef37aed9d17441fc14086ce62d1d40a8aae
유호영 <yoohoyeong@yuhoyeong-ui-MacBookPro.local>
2024-05-22 09:26:00 +0900

    fetch API

--- public/js/weather.js
+++ public/js/weather.js
@@ -5,38 +5,30 @@ import { tempsIcon, weatherIcon } from "./modules/KEY.js";
 const locationSuccess = (position) => {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  // *한국어로 받기
   const lang = 'kr';
-  // *온도 섭씨로 받기
   const units = 'metric';
-  console.log(lat, lon);
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKEY}&lang=${lang}&units=${units}`;
-  console.log(url);
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send();
-  xhr.onload = () => {
-    if(xhr.status === 200){
-      const APIweather = JSON.parse(xhr.response);
-      switch(APIweather.weather[0].main){
-        case 'Clouds' :
-          weatherIcon.clouds.forEach((icon) => {weather.weathers.classList.add(icon)});
-          break;
-        case 'Snow' :
-          weatherIcon.snow.forEach((icon) => {weather.weathers.classList.add(icon)});
-          break;
-        case 'Rain' : 
-          weatherIcon.rain.forEach((icon) => {weather.weathers.classList.add(icon)});
-        case 'Clear' :
-          weatherIcon.clear.forEach((icon) => {weather.weathers.classList.add(icon)});
-      }
-      tempsIcon.forEach((icon) => {weather.tempIcon.classList.add(icon)});
-      weather.tempText.innerText = `${APIweather.main.temp}도`;
-      weather.locations.innerText = APIweather.name;
-    } else {
-      console.error('Error', xhr.status, xhr.statusText);
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKEY}&lang=${lang}&units=${units}`;
+  fetch(url).then(Response => Response.json())
+  .then((APIweather) => {
+    console.log(APIweather);
+    switch(APIweather.weather[0].main){
+      case 'Clouds' :
+        weatherIcon.clouds.forEach((icon) => {weather.weathers.classList.add(icon)});
+        break;
+      case 'Snow' :
+        weatherIcon.snow.forEach((icon) => {weather.weathers.classList.add(icon)});
+        break;
+      case 'Rain' : 
+        weatherIcon.rain.forEach((icon) => {weather.weathers.classList.add(icon)});
+        break;
+      case 'Clear' :
+        weatherIcon.clear.forEach((icon) => {weather.weathers.classList.add(icon)});
+        break;
     }
-  }
+    tempsIcon.forEach((icon) => {weather.tempIcon.classList.add(icon)});
+    weather.tempText.innerText = `${APIweather.main.temp}도`;
+    weather.locations.innerText = APIweather.name;
+  });
 }
 
 const locationError = () => {
